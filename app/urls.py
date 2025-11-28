74e10f09dd19868002f5d1af8b23cbad116751a6
Nikson1122 <104704390+Nikson1122@users.noreply.github.com>
2025-11-28 12:48:16 +0545

    upto meeting completed

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('meeting/', views.meeting, name='meeting'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/base.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def meeting(request):
+    return render(request, 'app/meeting.html')
